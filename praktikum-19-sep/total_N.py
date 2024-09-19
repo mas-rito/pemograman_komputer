dffa70026692e81e401c6672bf221d45b1d696eb
Rito Ramadhan <ritoramadhan8@gmail.com>
2024-09-19 22:03:27 +0700

    menambahkan 1 percobaan pada praktikum 19 September

---
+++ praktikum-19-sep/total_N.py
@@ -0,0 +1,13 @@
+N = int(input("Masukkan jumlah data (N): "))
+
+total = 0
+
+for i in range(1, N+1):
+    data = int(input(f"Masukkan data ke-{i}: "))
+    total += data
+
+rata_rata = total / N
+
+
+print(f"Total penjumlahan: {total}")
+print(f"Rata-rata: {rata_rata}")
